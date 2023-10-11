f96427271232217d973eaf2593eaef35e151dd84
huan-cdm <huan151009@163.com>
2023-10-11 19:32:32 +0800

    新增扫描进程选项

--- config.py
+++ config.py
@@ -2,12 +2,10 @@
 linux_rule_dir = "./rule_file"
 #linux_rule_dir = "C:\\Users\\101201905004\\Desktop\\yara_scan\\rule_file"
 
+
+
 #内存扫描规则文件目录
 memory_rule_dir = './memory_rule_file'
-
-
-
-
 
 
 
@@ -15,20 +13,16 @@ memory_rule_dir = './memory_rule_file'
 linux_sample_dir = "./target_file"
 #linux_sample_dir = "C:\\Users\\101201905004\\Desktop\\yara_scan\\target_file"
 
+
+
 #windows系统样本文件目录
 #win_sample_dir = ".\\target_file"
 win_sample_dir = "C:\\Users\\101201905004\\Desktop"
 
 
 
-
 #扫描结果文件路径
 scan_result = "./result/resultfile.txt"
-
-
-
-
-
 
 
 
@@ -38,7 +32,15 @@ scan_result = "./result/resultfile.txt"
 mode=0
 
 
+
 #配置内存扫描和文件扫描
 #内存扫描 value=0
 #文件扫描 value=1
 fileormemory=0
+
+
+
+#配置进程选项,扫描全部进程较慢
+#指定进程(java.exe、python.exe、php.exe) value = 0
+#所有进程 value = 1
+pid_value = 0
--- yarascan.py
+++ yarascan.py
@@ -13,6 +13,7 @@ from config import mode
 from config import fileormemory
 from config import memory_rule_dir
 from config import scan_result
+from config import pid_value
 
 
 
@@ -38,7 +39,7 @@ def memory_file_list():
 	file_memory_list = []
 	#遍历规则文件存到列表
 	for root, dirs, files in os.walk(memory_rule_dir):
-		for file1 in tqdm(files,desc="内存扫描遍历规则文件存入列表"):
+		for file1 in tqdm(files,desc="遍历所有内存扫描规则"):
 			time.sleep(0.1)
 			file_path = os.path.abspath(os.path.join(root, file1))
 			file_memory_list.append(file_path)
@@ -117,21 +118,34 @@ def lin():
 
 
 
-#遍历win系统进行PID存到列表
+#遍历系统进程进行扫描
 def win_pid():
     win_pid_list = []
     processes = psutil.pids()
-    for process in tqdm(processes,desc="遍历所有进程ID"):
 
-        try:
-            process_info = psutil.Process(process)
-            if process_info.name() == 'python.exe' or process_info.name() == 'java.exe' or process_info.name() == 'php-cgi.exe':
-            	win_pid_list.append(process)
-          
-        except psutil.NoSuchProcess:
-            print(f"PID: {process} not found")
-       
+    if int(pid_value) == 0:
+        for process in tqdm(processes,desc="提取java,python,php进程"):
+    
+            try:
+                process_info = psutil.Process(process)
+                if process_info.name() == 'python.exe' or process_info.name() == 'java.exe' or process_info.name() == 'php-cgi.exe':
+                	win_pid_list.append(process)
+        
+            except psutil.NoSuchProcess:
+                print(f"PID: {process} not found")
+
+    elif int(pid_value) == 1:
+        for allpid in tqdm(processes,desc="提取所有进程"):
+            try:
+                win_pid_list.append(allpid)
+            except psutil.NoSuchProcess:
+                print(f"PID: {process} not found")
+
+    else:
+        print("配置文件pid_value字段只允许0/1")
+
     return win_pid_list
+
 
 
 
@@ -141,14 +155,15 @@ def win_pid():
 def win_memory():
 	print("运行在windows系统")
 	#规则文件
-	memory_rule_file = memory_file_list()
+	memory_rule_file = memory_file_list()    
 	#进程PID
 	memory_pid = win_pid()
+
 	#定义扫描结果列表
 	memory_list = []
-	for mi in tqdm(memory_rule_file,desc="遍历所有内存扫描相关规则"):
+	for mi in tqdm(memory_rule_file,desc="遍历所有内存规则进行扫描"):
 		time.sleep(0.1)
-		for mj in tqdm(memory_pid,desc="提取java,python,php进程进行扫描"):
+		for mj in tqdm(memory_pid,desc="遍历win_pid提取的进程进行扫描"):
 			time.sleep(0.1)
 			#判断是否打印字符串
 			if int(mode) == 0:
@@ -159,7 +174,6 @@ def win_memory():
 				memory_list.append(memory_win_result)
 			else:
 				print("配置文件mode字段只允许0/1")
-
 
 	#清空列表中为空的数据
 	memory_new_list = []
@@ -183,7 +197,6 @@ def win_memory():
 if __name__ == "__main__":
 
     if int(fileormemory) == 0:
-
         print("程序正在进行内存扫描......")
         if sys.platform.startswith('linux'):
         	print("暂不支持linux平台内存扫描")
@@ -192,9 +205,7 @@ if __name__ == "__main__":
         else:
             print("python运行在其他操作系统上")
 
-
     elif int(fileormemory) == 1:
-
         print("程序正在进行文件扫描......")
         if sys.platform.startswith('linux'):
             lin()
